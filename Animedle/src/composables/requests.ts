7f7a71c415ec611677597b709c71e3c3bb9e69c7
Mattox0 <matbegaaar@gmail.com>
2024-02-26 23:09:04 +0100

    Sort anime by title

--- Animedle/src/composables/requests.ts
+++ Animedle/src/composables/requests.ts
@@ -1,8 +1,9 @@
 import axios from 'axios'
+import type {Anime} from "@/types/Anime";
 
 export async function getAllAnime() {
 	const response = await axios.get(import.meta.env.VITE_BASE_URL + '/anime')
-  return response.data;
+  return response.data.sort((a: Anime, b: Anime) => a.title.localeCompare(b.title));
 }
 
 export async function getRandomAnime() {
