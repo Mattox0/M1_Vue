0cccf50a9a8b8a5fdedcb739bec43165bf44f03b
Mattox0 <matbegaaar@gmail.com>
2024-02-27 22:26:54 +0100

    Add vercel.json

--- Animedle/src/components/game/AppLivesBox.vue
+++ Animedle/src/components/game/AppLivesBox.vue
@@ -1,12 +1,12 @@
 <script lang="ts" setup>
-defineProps({
-  nbTry: Number,
-  health: Number
-})
+const props = defineProps<{
+  nbTry: number,
+  health: number
+}>()
 </script>
 <template>
   <div class="lives-box">
-    <img src="@/assets/images/heart-full.png" alt="lives" class="lives" v-for="index in health-nbTry" />
+    <img src="@/assets/images/heart-full.png" alt="lives" class="lives" v-for="index in (props.health-props.nbTry)" />
     <img src="@/assets/images/heart-empty.png" alt="lives" class="lives" :class="`${index.toString() === '1' ? 'empty' : ''}`" v-for="(index, elem) in nbTry">
   </div>
 </template>
